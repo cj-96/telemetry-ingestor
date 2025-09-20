57e1b1b3875f3df902adbd5d2d531715ff651b73
cj-96 <chamindujayakody1@gmail.com>
2025-09-21 02:43:07 +0530

    telemetry dto created

---
+++ src/telemetry/dto/create-telemetry.dto.ts
@@ -0,0 +1,27 @@
+// create-cat.dto.ts
+import { IsString, IsNumber, IsNotEmpty, IsISO8601 } from 'class-validator';
+
+class Metric {
+  @IsNumber()
+  @IsNotEmpty()
+  temperature!: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  humidity!: number;
+}
+
+export class CreateTelemetryDto {
+  @IsString()
+  @IsNotEmpty()
+  deviceId!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  siteId!: string;
+
+  @IsISO8601()
+  ts!: string;
+
+  metrics!: Metric;
+}
