8ab845df73d6f17ce6def1d0e5de1601b15ab92c
cj-96 <chamindujayakody1@gmail.com>
2025-09-21 02:41:31 +0530

    relavant middlewares added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.use(bodyParser.json({ limit: '100kb' }));
+  app.use(bodyParser.urlencoded({ limit: '100kb', extended: true }));
 }
 bootstrap();
